7099423b82d53d73f82efeea3a97ccb14fbac904
zexx-dev <padmabatibhol8@gmail.com>
2026-05-07 23:02:26 +0530

    Delete start.py

--- start.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.private & filters.command("start"))
-async def start(client, message):
-
-    await message.reply(
-        "✅ Bot Active Successfully"
-    )
