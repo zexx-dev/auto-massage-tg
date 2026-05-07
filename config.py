ab6d296bf9ca6d471b71dc26fcbbe195c4c8b644
zexx-dev <padmabatibhol8@gmail.com>
2026-05-07 22:49:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+ADMIN_ID = int(os.environ.get("ADMIN_ID"))
+
+FORCE_CHANNEL = os.environ.get("FORCE_CHANNEL")
